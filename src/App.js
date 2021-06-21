f50b031f03c1151ae74859a02c6cf9d5f6d9412c
Cyue19 <yue_cheryl@hotmail.com>
2021-06-21 14:25:19 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "bootstrap/dist/css/boostrap.min.css";
+
+mapboxgl.accessToken = "pk.eyJ1IjoiY3l1ZTE5IiwiYSI6ImNrcTBiMWNlNDAzMzUycHA0ejFoYW9jOW0ifQ.LMzBp9YK9S7_a76qnm_8-g";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: -26,
+      longitude: 28,
+      zoom: 10
+    }
+
+    this.mapElement = React.createRef();
+  }
+
+  componentDidMount() {
+    const {latitude, longitude, zoom} = this.state;
+
+    this.map = new mapboxgl.Map({
+      container: this.mapElement.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [longitude, latitude],
+      zoom: zoom
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        text
+        {/* <div ref={this.mapElement} className="map"></div> */}
+      </div>
+    );
+  }
+
 }
 
 export default App;
